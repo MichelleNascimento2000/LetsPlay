f03a346d30a533ca582e0cac1a1e1ee56d0fb833
MichelleNascimento2000 <michelle.nascimento.silva@outlook.com>
2022-08-16 10:55:19 -0300

    Primeiro commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  	appId: 'br.mastermimi.letsplayapp',
+  	appName: 'Let\'s Play',
+  	webDir: 'www',
+  	bundledWebRuntime: false
+};
+
+export default config;
