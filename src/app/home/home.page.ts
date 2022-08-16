87a0909c1d3c178ce38309e33b7470d32df22537
MichelleNascimento2000 <michelle.nascimento.silva@outlook.com>
2022-08-16 11:44:40 -0300

    Criação da base da página de pesquisa de jogos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
     selector: 'app-home',
@@ -7,9 +9,12 @@ import { Component } from '@angular/core';
 })
 
 export class HomePage {
-    constructor(){}
+    constructor(
+        public router: Router
+    ){}
 
     public redirectToGameSearching(){
+		this.router.navigate(['search']);
 	}
 
 	public redirectToMyGameplays(){
